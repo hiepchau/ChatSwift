33d887adda30f23513f406e1900cca85244d14b8
Hiệp <ch.dhiep@gmail.com>
2022-12-08 00:31:07 +0700

    init UI

--- ChatSwift/Controllers/ChatViewController.swift
+++ ChatSwift/Controllers/ChatViewController.swift
@@ -6,24 +6,51 @@
 //
 
 import UIKit
+import MessageKit
 
-class ChatViewController: UIViewController {
+struct Message: MessageType {
+    var sender: SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKind
+}
 
+struct Sender: SenderType{
+    var senderId: String
+    var displayName: String
+}
+
+class ChatViewController: MessagesViewController {
+
+    private var messages = [Message]()
+    private let selfSender = Sender(senderId: "1", displayName: "hip")
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .blue
+        messages.append(Message(sender: selfSender,
+                                messageId: "1",
+                                sentDate: Date(),
+                                kind: .text("First message")))
+        messages.append(Message(sender: selfSender,
+                                messageId: "2",
+                                sentDate: Date(),
+                                kind: .text("First message1")))
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate{
+    func currentSender() -> SenderType {
+        return selfSender
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
     }
-    */
-
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
 }
--- ChatSwift/Controllers/LoginTableViewController.swift
+++ ChatSwift/Controllers/LoginTableViewController.swift
@@ -6,24 +6,48 @@
 //
 
 import UIKit
-
-class LoginTableViewController: UIViewController {
+import JGProgressHUD
+class LoginTableViewController: UITableViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    private let spinner = JGProgressHUD(style: .dark)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        usernameField.delegate = self
+        passwordField.delegate = self
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginButtonTapped(){
+        usernameField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        guard let email = usernameField.text, let password = passwordField.text, !email.isEmpty, password.count >= 6 else {
+            alertUserLoginError()
+            return
+        }
+//        spinner.show(in: view)
+//        performSegue(withIdentifier: "segue", sender: self)
     }
-    */
+    
 
+    func alertUserLoginError() {
+        let alert = UIAlertController(title: "Error", message: "Please fill information", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Dissmiss", style: .cancel, handler: nil))
+        present(alert, animated: true)
+    }
+}
+
+extension LoginTableViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        if textField == usernameField {
+            passwordField.becomeFirstResponder()
+        }
+        else if textField == passwordField {
+            loginButtonTapped()
+        }
+        return true
+    }
 }
